0dfd7ff537ec8599a4893c079f4629e1230acf10
Salvador Girones <salvadorgirones@gmail.com>
2023-12-11 17:34:44 +0100

    foundationdb-sys crate patch

--- docker/build-binary.sh
+++ docker/build-binary.sh
@@ -9,12 +9,12 @@ ROOT_DIR=$(dirname $(dirname $(realpath $0)))
 cd $ROOT_DIR
 
 if [[ $(uname -m) == "x86_64" ]]; then
-    export CARGO_EXTRA_FEATURES="$CARGO_EXTRA_FEATURES,storage-fdb"
+    export CARGO_EXTRA_FEATURES="http-compression,storage-tikv,storage-fdb"
     export DOCKER_BUILD_TARGET="builder-fdb" # Target for the binary builder
 else
     echo "###"
     echo "###> WARNING!"
-    echo "###> This architecture ('$(uname -m)') doesn't support FoundationDB."
+    echo "###> This architecture ('$(uname -m)') doesn't support FoundationDB. Don't build with storage-fdb feature."
     echo "###"
 
     export CARGO_EXTRA_FEATURES="http-compression,storage-tikv"
@@ -23,11 +23,16 @@ fi
 
 echo
 echo "###"
-echo "###> Build SurrealDB binary"
+echo "###> Build SurrealDB binary (with features: default,${CARGO_EXTRA_FEATURES})"
 echo "###"
 echo
 $DOCKER_BINARY build -t surrealdb-local/builder --target $DOCKER_BUILD_TARGET -f docker/Dockerfile .
-$DOCKER_BINARY run --rm -v $(pwd):/surrealdb -w /surrealdb surrealdb-local/builder cargo build --features ${CARGO_EXTRA_FEATURES}
+$DOCKER_BINARY run \
+    --rm \
+    -v $(pwd):/surrealdb \
+    -w /surrealdb \
+    surrealdb-local/builder \
+    bash -c "source /opt/rh/gcc-toolset-13/enable && cargo build --features ${CARGO_EXTRA_FEATURES}"
 
 echo "###"
 echo "###> Build SurrealDB docker images"
